6628af4234e2ac3e7f6239c8602b80a0a7b2ada0
marieleponti <marieleponti@gmail.com>
2022-08-31 11:34:25 -0500

    templates update

--- blog/views.py
+++ blog/views.py
@@ -1,9 +1,24 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 
+post = [
+    {
+        'author': 'Mari',
+        'title': 'Blog Post 1',
+        'content': 'Everything is content.',
+        'date_posted': '31 August 2022'
+    },
+    {
+        'author': 'Jesse',
+        'title': 'Self care and empowerment',
+        'content': 'Collective care and real power.',
+        'date_posted': '1 September 2022'
+    }
+]
+
 
 def home(request):
-    return HttpResponse('<h1>Blog home</h1>')
+    return render(request, 'blog/home.html')
 
 def about(request):
-    return HttpResponse('<h1>About!!</h1>')
+    return render(request, 'blog/about.html')
