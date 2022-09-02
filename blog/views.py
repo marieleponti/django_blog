0491562333b952a5a37bbae80f893ada42e56621
marieleponti <marieleponti@gmail.com>
2022-09-02 11:40:51 -0500

    templates, databases, migrations

--- blog/views.py
+++ blog/views.py
@@ -1,24 +1,13 @@
 from django.shortcuts import render
 from django.http import HttpResponse
-
-post = [
-    {
-        'author': 'Mari',
-        'title': 'Blog Post 1',
-        'content': 'Everything is content.',
-        'date_posted': '31 August 2022'
-    },
-    {
-        'author': 'Jesse',
-        'title': 'Self care and empowerment',
-        'content': 'Collective care and real power.',
-        'date_posted': '1 September 2022'
-    }
-]
+from .models import Post
 
 
 def home(request):
-    return render(request, 'blog/home.html')
+    context = {
+        'posts': Post.objects.all()
+    }
+    return render(request, 'blog/home.html', context)
 
 def about(request):
-    return render(request, 'blog/about.html')
+    return render(request, 'blog/about.html', {'title': 'About'})
