f724e69f5eb17811813fd52493f48e45d3a08f11
marieleponti <marieleponti@gmail.com>
2022-09-20 11:24:49 -0500

    Update user profile, add author profile image to blog posts

--- users/models.py
+++ users/models.py
@@ -2,6 +2,7 @@ from operator import mod
 from pyexpat import model
 from django.db import models
 from django.contrib.auth.models import User
+from PIL import Image 
 
 class Profile(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE)
@@ -9,6 +10,17 @@ class Profile(models.Model):
 
     def __str__(self):
         return f'{self.user.username} Profile'
+
+    def save(self):
+        super().save()
+
+        img = Image.open(self.image.path)
+
+        if img.height > 300 or img.width > 300:
+            output_size = (300, 300)
+            img.thumbnail(output_size)
+            img.save(self.image.path)
+
 
 
 
