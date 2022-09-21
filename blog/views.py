ccb7220ac006400aa85f861421649a4f01b5bf24
marieleponti <marieleponti@gmail.com>
2022-09-21 11:51:43 -0500

    Added ListView, DetailView, CreateView; New Post form (start)

--- blog/views.py
+++ blog/views.py
@@ -1,4 +1,5 @@
 from django.shortcuts import render
+from django.views.generic import ListView, DetailView, CreateView
 from django.http import HttpResponse
 from .models import Post
 
@@ -8,6 +9,19 @@ def home(request):
         'posts': Post.objects.all()
     }
     return render(request, 'blog/home.html', context)
+
+class PostListView(ListView):
+    model = Post
+    template_name ='blog/home.html'
+    context_object_name = 'posts'
+    ordering = ['-date_posted']
+
+class PostDetailView(DetailView):
+    model = Post
+
+class PostCreateView(CreateView):
+    model = Post
+    fields = ['title', 'content']
 
 def about(request):
     return render(request, 'blog/about.html', {'title': 'About'})
