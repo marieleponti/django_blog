0b7e59b59168422a32d6f45a9ba787acfb3bd994
marieleponti <marieleponti@gmail.com>
2022-09-23 11:55:14 -0500

    Added post-create, post-update, post-delete functionality, paths, buttons. Only allows post author to update and delete.

--- blog/models.py
+++ blog/models.py
@@ -1,3 +1,4 @@
+from django.urls import reverse
 from django.db import models
 from django.utils import timezone
 from django.contrib.auth.models import User
@@ -10,4 +11,7 @@ class Post(models.Model):
     author = models.ForeignKey(User, on_delete=models.CASCADE)
 
     def __str__(self):
-        return self.title
+        return self.title   
+
+    def get_absolute_url(self):
+        return reverse('post-detail', kwargs={'pk': self.pk})
